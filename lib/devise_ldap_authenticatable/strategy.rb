f9df980a6635765676b4c51317e5472425998fbc
Gilli (Michael Gilliland) <gillilandmj@upmc.edu>
2014-07-15 11:34:12 -0400

    Added a log statement for debugging authentication_hash

--- lib/devise_ldap_authenticatable/strategy.rb
+++ lib/devise_ldap_authenticatable/strategy.rb
@@ -4,6 +4,7 @@ module Devise
   module Strategies
     class LdapAuthenticatable < Authenticatable
       def authenticate!
+        Rails.logger.error "#{authentication_hash.inspect}"
         resource = mapping.to.find_for_ldap_authentication(authentication_hash.merge(password: password))
         
         if resource && validate(resource) { resource.valid_ldap_authentication?(password) }
